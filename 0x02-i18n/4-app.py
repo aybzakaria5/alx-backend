2624ef134b9f962ba22232e9a854090e56e428cf
aybzakaria5 <ayoub.don.zakaria@gmail.com>
2024-05-18 00:38:09 +0000

    fixing reqs and some errors

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-"""working on task 4"""
-from flask import Flask, request, render_template
-from flask_babel import Babel, gettext as _
-from typing import Optional
+"""task 4 """
+from flask import Flask, render_template, request
+from flask_babel import Babel
 
 
 class Config(object):
-    """Configuration for the Flask app"""
+    """configiration class"""
 
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
@@ -19,27 +18,16 @@ babel = Babel(app)
 
 
 @babel.localeselector
-def get_locale() -> Optional[str]:
-    """
-    Select the best matching locale from the request.
-    Returns:
-        The locale if found in the request arguments and supported locales.
-        Otherwise, returns the best match from the accepted languages.
-    """
-    locale = request.args.get('locale')
-    if locale in app.config["LANGUAGES"]:
-        return locale
+def get_locale() -> str:
+    """getting localization to displaying language """
+    if request.args.get("locale") in app.config["LANGUAGES"]:
+        return request.args.get("locale")
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
 @app.route("/")
 def index() -> str:
-    """
-    Render the index page.
-
-    Returns:
-        The rendered HTML template for the index page.
-    """
+    """rendering the-html """
     return render_template("4-index.html")
 
 
